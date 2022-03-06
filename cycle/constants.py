a7dfca6bb49225e26c409c16d5a4dd63a4514d4f
juliwhite <julietawhite30@gmail.com>
2022-03-05 21:59:33 -0700

    add constants

---
+++ cycle/constants.py
@@ -0,0 +1,16 @@
+
+from tkinter.tix import COLUMN
+from game.shared.color import Color
+
+COLUMNS = 40
+ROWS = 20
+CELL_SIZE = 15
+MAX_X = 900
+MAX_Y = 600
+FRAME_RATE = 15
+FONT_SIZE = 15
+CAPTION = "Cycle"
+CYCLE_LENGTH = 4
+WHITE = Color(255, 255, 255)
+RED = Color(255, 0, 0)
+GREEN = Color(0, 255, 0)
